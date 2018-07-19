3b21ba420044a558131c4a3d626bdead6cf0b6be
vacuole1989 <154034117@qq.com>
2018-07-19 21:36:02 +0800

    erterter

--- src/main/java/com/cxd/xiaonuan/controller/AppTagController.java
+++ src/main/java/com/cxd/xiaonuan/controller/AppTagController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Date;
-import java.util.List;
 
 @Controller
 public class AppTagController {
@@ -44,11 +43,14 @@ public class AppTagController {
         appTag.setPushBizId(DateUtil.format(new Date()));
         appTag.setPushKey(1);
 
-        Integer save = baseMysqlCRUDManager.save(appTag);
-        PageResultCommand<AppTag> query = baseMysqlCRUDManager.query(appTag);
+//        Integer save = baseMysqlCRUDManager.save(appTag);
+        for (int i = 0; i <10000 ; ++i) {
 
-        List<AppTag> all = appTagMapper.findAll(appTag);
-        List<AppTag> all1 = appTagService.findAll(appTag);
+            PageResultCommand<AppTag> query = baseMysqlCRUDManager.query(appTag);
+        }
+
+//        List<AppTag> all = appTagMapper.findAll(appTag);
+//        List<AppTag> all1 = appTagService.findAll(appTag);
 
         return "/index";
     }
